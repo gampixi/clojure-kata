469b1b95e1bf19daae14ea129d0ed14f87b398c2
Rūdolfs Agris Stilve <rudolfsagris@gmail.com>
2021-11-09 20:56:29 +0200

    ex7

--- src/kata/exercise7.clj
+++ src/kata/exercise7.clj
@@ -6,15 +6,14 @@
 
   ; Calculate the average of customer ages by using your own (avg ...)
   ;; Don't forget about (apply ...)
-  (let [avg-age -1]
+  (let [avg-age ((fn [x] (/ (apply + x) (count x))) (map :age (:customers mall)))]
 
     (is (= avg-age (rationalize 28.7)))))
-
 
 (testing "How Much for Everything?!"
 
   ; Calculate the sum of the prices for all the items in all shops by using (flatten ...)
   ;; Don't forget about (apply ...)
-  (let [total -1]
+  (let [total (apply + (map :price (flatten (map :items (:shops mall)))))]
 
     (is (= total 60930))))
